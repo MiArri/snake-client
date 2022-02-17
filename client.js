2a833e97cf66452e70f259ee6c72b419c12d070a
Elena Chernikova <lena@acvos.com>
2022-02-16 20:36:41 -0800

    constants added

--- client.js
+++ client.js
@@ -1,12 +1,13 @@
 const { Socket } = require("dgram");
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 // establishes a connection with the game server
 const connect = function (callback) {
   console.log("Connecting ...");
   const conn = net.createConnection({
-    host: "165.227.47.243", // IP address here,
-    port: 50541 // PORT number here,
+    host: IP, // IP address here,
+    port: PORT, // PORT number here,
   }, () => {
     console.log('Successfully connected to game server!');
     callback();
